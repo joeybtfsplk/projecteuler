0b36f1e1b42f57377246c46d2eac3f40afe615e9
Terry Sanders <wa9wne@att.net>
2014-08-01 19:18:06 -0400

    10.py - Answered on Fri Aug  1 19:18:21 EDT 2014

--- 10.py
+++ 10.py
@@ -6,62 +6,45 @@ from: Project Euler: http://projecteuler.net
 auth: tls
 purp: The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17. Find the sum of 
       all the primes below two million.
-      Ans: 
-      The sum of all primes from 2 to 1,999,993 is 142,913,828,922
+      Ans: 142913828922 on Fri Aug  1 19:15:40 EDT 2014
 """
-
-def primes(n):
+def prime_list(num):
     """
-    Return list of prime factors of n.
-    If list == [] n is a prime.
+    From: http://en.wikibooks.org/wiki/Efficient_Prime_Number_Generating_
+    Algorithms
+    Modified: tls, fixed so entering 10 did not return 11 in the list of primes.
+    Return primes up to but not including num.
     """
-    divisors = [ d for d in xrange(2,n//2+1) if n % d == 0 ]
-    return [ d for d in divisors if \
-             all( d % od != 0 for od in divisors if od != d ) ]
-
-def xprime_sieve(num):
-    summed= 2 + 3 + 5 + 7
-    prime_list= [2,3,5,7]
-    for i in xrange(8,num):
-        if i % 2== 0 or i % 3== 0 or i % 5== 0 or i % 7== 0:
-            next
-        else:
-            ans= primes(i)
-            if len(ans) == 0:
-                summed += i
-                prime_list.append(i)
-                #print i
-    print prime_list
-    return summed
-
-def prime_sieve(num):
-    """
-    try this one out
-    """
-    summed= 0
-    prime_list= [2]
-    for i in xrange(3,num,2):
-        for p in prime_list:
-            if i % p == 0:
-                next
-        else:
-            ans= primes(i)
-            if len(ans) == 0:
-                prime_list.append(i)
-                summed += i
-                last_prime= i
-                #print i
-    print("num= %d, last prime: %d" % (num,last_prime))
-    print prime_list
-    return summed
+    num= int(num)
+    if num <= 2:
+        return []
+    tp= [2]    # fills up with primes
+    pp= 3        # potential primes
+    while pp < num:
+        test= True
+        sqrtpp= int(pp**0.5)
+        for a in tp:
+            if a > sqrtpp: # can be no more compoents from here on
+                break
+            if pp % a == 0:# well, it's divisible by something
+                test= False
+                break
+        if test: 
+            tp.append(pp)
+        pp += 2
+    return tp
 
 def main():
     """
     """
-    num= 10
     num= 2000000
-    ans= prime_sieve(num)
-    print("Sum: %d" % ans)
+    ans= prime_list(num)
+    print("Sum of primes from 2 to %d: %d" % (num, sum(ans)))
+    print("Found a total of %d primes." % len(ans))
+    if len(ans) <= 6:
+        print("Primes: %s" % str(ans))
+    else:
+        print("From %s... to ...%s" % (str(ans[:3]),ans[-3:]))
     return 0
 
 if __name__ == "__main__":

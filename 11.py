a2089ad6b5907ea39d7b2367b70e6999d083fcbb
Terry Sanders <wa9wne@att.net>
2014-08-03 19:21:21 -0400

    11.py - Done with right diagonal calcs

--- 11.py
+++ 11.py
@@ -30,28 +30,69 @@ purp: In the 20x20 grid below, four numbers along a diagonal line have been mark
     (up, down, left, right, or diagonally) in the 20x20 grid?
     Ans: 
 """
-"""
-    "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08"
-    "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00"
-    "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65"
-    "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91"
-    "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80"
-    "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50"
-    "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70"
-    "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21"
-    "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72"
-    "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95"
-    "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92"
-    "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57"
-    "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58"
-    "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40"
-    "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66"
-    "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69"
-    "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36"
-    "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16"
-    "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54"
-    "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
-"""
+def xset_up_matrix():
+    matrix= []
+    row= "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08"
+    row= to_int(row)
+    matrix.append(row)
+    row= "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00"
+    row= to_int(row)
+    matrix.append(row)
+    row= "81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65"
+    row= to_int(row)
+    matrix.append(row)
+    row= "52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91"
+    row= to_int(row)
+    matrix.append(row)
+    row= "22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80"
+    row= to_int(row)
+    matrix.append(row)
+    row= "24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50"
+    row= to_int(row)
+    matrix.append(row)
+    row= "32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70"
+    row= to_int(row)
+    matrix.append(row)
+    row= "67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21"
+    row= to_int(row)
+    matrix.append(row)
+    row= "24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72"
+    row= to_int(row)
+    matrix.append(row)
+    row= "21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95"
+    row= to_int(row)
+    matrix.append(row)
+    row= "78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92"
+    row= to_int(row)
+    matrix.append(row)
+    row= "16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57"
+    row= to_int(row)
+    matrix.append(row)
+    row= "86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58"
+    row= to_int(row)
+    matrix.append(row)
+    row= "19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40"
+    row= to_int(row)
+    matrix.append(row)
+    row= "04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66"
+    row= to_int(row)
+    matrix.append(row)
+    row= "88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69"
+    row= to_int(row)
+    matrix.append(row)
+    row= "04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36"
+    row= to_int(row)
+    matrix.append(row)
+    row= "20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16"
+    row= to_int(row)
+    matrix.append(row)
+    row= "20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54"
+    row= to_int(row)
+    matrix.append(row)
+    row= "01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
+    row= to_int(row)
+    matrix.append(row)
+    return matrix
 
 def to_int(row):
     row= row.split()
@@ -60,6 +101,14 @@ def to_int(row):
     return row
 
 def set_up_matrix():
+    """
+    First solve problem using a 5x5 matrix.
+    1  9 3 4 5
+    2 30 4 5 6
+    3  8 4 5 7
+    4  5 6 7 8
+    5  6 7 8 9
+    """
     matrix= []
     row= "1 9 3 4 5"
     row= to_int(row)
@@ -101,6 +150,9 @@ def maxinrow(row,span=2):
     return maximum
 
 def maxofrows(matrix,span=2):
+    """
+    Go through the rows from top to bottom and return the maximum.
+    """
     maximum= 0
     for i in range(0,len(matrix)):
         #print maxinrow(matrix[i],span)
@@ -108,10 +160,54 @@ def maxofrows(matrix,span=2):
         maximum = ans if ans > maximum else maximum
     return maximum
 
+def maxdiagl2r(matrix,span=2):
+    """
+    Get ul to lr diag moving across top row.
+    Then get ul to lr diag moving down left column.
+    Return maximum product found.
+    """
+    print("start across columns")
+    for i in matrix:
+        print i
+    maximum= 0
+    end= len(matrix)
+    start= 0
+    col = 0
+    while not ((len(matrix) - start) < span):
+        row= []
+        for r in range(0,end):
+            row.append(matrix[r][r+ col])
+        print "> ", row
+        ans= maxinrow(row,span)
+        maximum = ans if ans > maximum else maximum
+        col += 1
+        end -= 1
+        start += 1
+    print("start down rows")
+    end= len(matrix)
+    rowstep= 0
+    col= 0
+    start= 0
+    while not ((len(matrix) - start) < span):
+        row= []
+        col= 0
+        for r in range(rowstep,end):
+            #print("r= %d, col= %d, end= %d" % (r, col, end))
+            row.append(matrix[r][col])
+            col += 1
+        print "> ", row
+        ans= maxinrow(row,span)
+        maximum = ans if ans > maximum else maximum
+        rowstep += 1
+        #end -= 1
+        start += 1
+    return maximum
+
 def main():
     """
+    First solve problem using a 5x5 matrix.
     """
-    span= 2
+    span= 4
     matrix= set_up_matrix()
     print matrix
     maximum= 0
@@ -119,11 +215,29 @@ def main():
     ans= maxofrows(matrix,span)
     maximum = ans if ans > maximum else maximum
     print maximum   # maximum value along the rows
+
+    # transpose columns to rows and call maxofrows again
     matrixT= zip(matrix[0],matrix[1],matrix[2],matrix[3],matrix[4])
+    #matrixT= zip(matrix[0],matrix[1],matrix[2],matrix[3],matrix[4],matrix[5],matrix[6],matrix[7],\
+    #matrix[8],matrix[9],matrix[10],matrix[11],matrix[12],matrix[13],matrix[14],matrix[15],matrix[16],\
+    #matrix[17],matrix[18],matrix[19])
     print matrixT
     ans= maxofrows(matrixT,span)
     maximum = ans if ans > maximum else maximum
     print maximum   # maximum value along rows and columns
+
+    # now work on going diagonally from upper left to lower  right
+    # create a row from the longest diagonal
+    # process it for maximum value
+    # move to next column starting at row 0 and get a row from that diagonal
+    # stop moving to the right when span will be greater than the elements in
+    # the row to be newly created.
+    # now go from ...
+    ans= maxdiagl2r(matrix,span)
+    print ans
+    return 0
+    # Now go from upper right to lower left getting rows looking for maximum.
+    ans= maxdiagr2l(matrix)
     return 0
 
 if __name__ == "__main__":
